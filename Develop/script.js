e6b383a865953123ab8ef4ae071609311de209d9
unknown <jwade1327@gmail.com>
2020-07-15 15:14:37 -0500

    javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,64 +1,44 @@
-function getLocalStorage(key) {
-    let value = localStorage.getItem(key);
-    if (value) {
-        $(`#text${key}`).text(value);
-    }
-}
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        var value = $(this).siblings(".info").val();
+        var time = $(this).parent().attr("id");
 
-$( document ).ready(function() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-    for (let i = 9; i < 18; i++) {
+        localStorage.setItem(time, value);
+    });
+
+    function currentTime() {
+        var hourNow = moment().hours();
     
-        // create a row
-        var row = $(`<div data-time=${i} id='${i}' class="row">`);
+    $(".time-block").each(function() {
+        var timeBlock = parseInt($(this).attr("id").split("-")[1]);
 
-        // create a column
-        var col1 = $('<div class="col-sm-2"> <p class="hour">' + formatAMPM(i) + '</p>');
-
-        //create column 2
-        var col2 = $(`<div class="col-sm-8 past"><textarea id=text${i} class="description" placeholder="Add your event here..."></textarea>`);        
-       
-        //create column 3
-        var col3 = $(`<div class="col-sm-2"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button>`)
-        
-        // append col to row
-        row.append(col1);
-        row.append(col2);
-        row.append(col3);
-
-        // last step add rows to container
-        $(".container").append(row);
-
-        getLocalStorage(i);
-    }
-
-    function formatAMPM(hours) {
-        var ampm = hours >= 12 ? 'pm' : 'am';
-        hours = hours % 12;
-        hours = hours ? hours : 12;
-        return hours + ampm;
-    }
-formatAMPM();
-
-function updateColors(){
-        var currentTime = new Date().getHours();
-        for (var i = 9; i < 18; i++) { 
-        console.log(currentTime, $(`#${i}`).data("time"));
-         if ($(`#${i}`).data("time") == currentTime){
-            $(`#text${i}`).addClass( "present");
-        } else if (currentTime < $(`#${i}`).data("time")) {
-            $(`#text${i}`).addClass( "future");
-        }
-    }
+        if (timeBlock < hourNow) {
+            $(this).addClass("past");
+        } else if (timeBlock === hourNow) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            } 
+    });
 }
 
-setInterval(function() {
-    updateColors();
-}, 1000);
+currentTime();
 
-var saveBtn = $('.saveBtn');
-saveBtn.on('click', function(){
-    let eventId = $(this).attr('id');
-    let eventText = $(this).parent().siblings().children('.description').val();
-    localStorage.setItem(eventId, eventText);
-});});
+var interval = setInterval(currentTime, 10000);
+
+$("#currentTime").text(moment().format("dddd, MMM Do"));
+
+$("#hour-9.info").val(localStorage.getItem("hour-9"));
+$("#hour-10.info").val(localStorage.getItem("hour-10"));
+$("#hour-11.info").val(localStorage.getItem("hour-11"));
+$("#hour-12.info").val(localStorage.getItem("hour-12"));
+$("#hour-13.info").val(localStorage.getItem("hour-13"));
+$("#hour-14.info").val(localStorage.getItem("hour-14"));
+$("#hour-15.info").val(localStorage.getItem("hour-15"));
+$("#hour-16.info").val(localStorage.getItem("hour-16"));
+$("#hour-17.info").val(localStorage.getItem("hour-17"));
+
+});
