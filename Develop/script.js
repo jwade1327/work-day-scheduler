26955c09786329814695bbe1952c305c1c75cdc5
unknown <jwade1327@gmail.com>
2020-07-19 22:44:19 -0500

    javascript colors still not linking

--- Develop/script.js
+++ Develop/script.js
@@ -1,44 +1,45 @@
-$(document).ready(function() {
-    $(".saveBtn").on("click", function() {
-        var value = $(this).siblings(".info").val();
-        var time = $(this).parent().attr("id");
-
-        localStorage.setItem(time, value);
-    });
-
-    function currentTime() {
-        var hourNow = moment().hours();
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var time = moment().format("dddd, MMMM Do, hh:mm a");
+        console.log("time: ", time);
     
-    $(".time-block").each(function() {
-        var timeBlock = parseInt($(this).attr("id").split("-")[1]);
-
-        if (timeBlock < hourNow) {
-            $(this).addClass("past");
-        } else if (timeBlock === hourNow) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
-            } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
-            } 
+        $("#currentDay").append(time);
     });
-}
 
-currentTime();
+    function updateHour() { 
 
-var interval = setInterval(currentTime, 10000);
+        var hourNow = moment().hour();
 
-$("#currentTime").text(moment().format("dddd, MMM Do"));
+        $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        if (blockHour < hourNow) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === hourNow) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+    
+    updateHour();
+       
+    $("#currentDay").text(moment().format("dddd, MMMM Do, hh:mm a"));
 
-$("#hour-9.info").val(localStorage.getItem("hour-9"));
-$("#hour-10.info").val(localStorage.getItem("hour-10"));
-$("#hour-11.info").val(localStorage.getItem("hour-11"));
-$("#hour-12.info").val(localStorage.getItem("hour-12"));
-$("#hour-13.info").val(localStorage.getItem("hour-13"));
-$("#hour-14.info").val(localStorage.getItem("hour-14"));
-$("#hour-15.info").val(localStorage.getItem("hour-15"));
-$("#hour-16.info").val(localStorage.getItem("hour-16"));
-$("#hour-17.info").val(localStorage.getItem("hour-17"));
+    $("#hour-9 .info").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .info").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .info").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .info").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .info").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .info").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .info").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .info").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .info").val(localStorage.getItem("hour-17"));
 
 });
